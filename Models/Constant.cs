c464584c5507fe8e675428e619afd4d7c5a19d19
Pavitha Shanmugam <M1033925@mindtree.com>
2022-11-09 22:12:52 +0530

    first commit

---
+++ Models/Constant.cs
@@ -0,0 +1,10 @@
+﻿namespace IOTDeviceApp.Models
+{
+    public static class Constant
+    {
+        public static string IOTHubConnString = "HostName=aziothub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=xa4HJoi2vpTgtsk3rY2uL7+OE2CiwMreKepy0k6IRls=";
+        public static string IOTDeviceConnString = "HostName=aziothub.azure-devices.net;DeviceId=myiotdevice;SharedAccessKey=OxfjOtbyDA1M1l1cw/XDNTff+/KzKdW6qfJYpxlodrE=";
+        public static string IOTDevUpdatConnString = "HostName=aziothub.azure-devices.net;DeviceId=myiotdevice;SharedAccessKey=OxfjOtbyDA1M1l1cw/XDNTff+/KzKdW6qfJYpxlodrE=";
+
+    }
+}
